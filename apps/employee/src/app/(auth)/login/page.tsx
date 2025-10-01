c673deba32a092700796fcc94d194decb3a25cbe
genki asai <asai001@efficient-technology.com>
2025-10-01 22:27:59 +0900

    ログイン画面レスポンシブ対応 & アイコンの追加

--- apps/employee/src/app/(auth)/login/page.tsx
+++ apps/employee/src/app/(auth)/login/page.tsx
@@ -3,11 +3,18 @@ import LoginForm from "apps/employee/src/components/auth/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-gradient-to-br from-blue-200  via-teal-50 to-blue-200">
-      <Image className="mt-24" src="/correcre-logo.svg" alt="" width={160} height={37} />
-      <div className="mt-16 w-[400px]">
+    <div className="flex min-h-dvh flex-col items-center bg-gradient-to-br from-blue-200  via-teal-50 to-blue-200">
+      <Image className="mt-12 lg:mt-24" src="/correcre-logo.svg" alt="" width={160} height={37} />
+      <div className="mt-12 w-[400px]">
         <LoginForm></LoginForm>
       </div>
+      <Image
+        className="absolute bottom-5 right-7.5 lg:bottom-15 lg:right-20 w-16 h-auto lg:w-[110px]"
+        src="/correcre-icon.svg"
+        alt=""
+        width={110}
+        height={66}
+      />
     </div>
   );
 }
