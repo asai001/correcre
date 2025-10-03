b1ad3d033ef0992372368cc42dd6609fbd031913
genki asai <asai001@efficient-technology.com>
2025-10-03 19:16:03 +0900

    ダッシュボードの理念エリア実装

---
+++ apps/employee/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Philosophy from "@employee/components/dashboard/philosophy";
+
+export default function DashboardPage() {
+  return (
+    <div className="container mx-auto px-6">
+      <div className="pt-5">
+        <Philosophy />
+      </div>
+    </div>
+  );
+}
---
+++ apps/employee/src/components/dashboard/philosophy.tsx
@@ -0,0 +1,16 @@
+export default function Philosophy() {
+  return (
+    <div className="bg-white rounded-2xl shadow-lg p-6 mb-8 space-y-4 lg:space-y-6">
+      <div className="flex flex-col justify-center items-center text-center">
+        <div className="text-md lg:text-xl text-gray-600">経営理念</div>
+        <div className="text-md font-bold text-gray-800 lg:text-2xl mt-1">
+          そうぞうの力で未来を描く 想像力と創造力で可能性を切り拓き続ける
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center text-center">
+        <div className="text-md lg:text-xl text-gray-600">PURPOSE</div>
+        <div className="text-md font-bold text-gray-800 lg:text-2xl mt-1">中小企業の魅力を引き出し国力を上げる</div>
+      </div>
+    </div>
+  );
+}
