2b0b47f317555d3fb30722436883c2d30eb0cac6
genki asai <asai001@efficient-technology.com>
2025-10-03 16:07:29 +0900

    Vercel デプロイ用設定値の修正

--- apps/employee/next.config.ts
+++ apps/employee/next.config.ts
@@ -2,6 +2,10 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
-  transpilePackages: ["@correcre/validation", "@correcre/types", "@correcre/adapters"],
+  transpilePackages: ["@correcre/validation", "@correcre/types", "@correcre/adapters", "@correcre/theme"],
+  experimental: {
+    // apps/employee の外にある TS を読むため
+    externalDir: true,
+  },
 };
 export default nextConfig;
