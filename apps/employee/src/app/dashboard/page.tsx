914db95cecad54538699434f222e9c6e076ff2ce
genki asai <asai001@efficient-technology.com>
2025-10-07 17:09:46 +0900

    従業員側ダッシュボードのポイント確認エリア実装忘れ

--- apps/employee/src/app/dashboard/page.tsx
+++ apps/employee/src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import Philosophy from "@employee/components/dashboard/Philosophy";
 import User from "@employee/components/dashboard/User";
+import DispCard from "@employee/components/dashboard/DispCard";
 
 export default function DashboardPage() {
   return (
@@ -9,6 +10,11 @@ export default function DashboardPage() {
       </div>
       <div>
         <User />
+      </div>
+      <div className="flex flex-col justify-between lg:flex-row">
+        <DispCard icon="/coin.svg" title="現在の保有ポイント" number={2450} unit="ポイント" color="#D97706" />
+        <DispCard icon="/calendar.svg" title="先月の獲得ポイント" number={380} unit="ポイント" color="#059669" />
+        <DispCard icon="/trophy.svg" title="今月の達成割合" number={70} unit="パーセント" color="#2563EB" />
       </div>
     </div>
   );
