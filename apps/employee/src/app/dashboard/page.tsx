58964732f2b3cf54f0d3621a274ef1882b9df5d0
genki asai <asai001@efficient-technology.com>
2025-10-07 16:54:40 +0900

    従業員側ダッシュボードのユーザー情報エリア実装

--- apps/employee/src/app/dashboard/page.tsx
+++ apps/employee/src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import Philosophy from "@employee/components/dashboard/Philosophy";
+import User from "@employee/components/dashboard/User";
 
 export default function DashboardPage() {
   return (
     <div className="container mx-auto px-6">
       <div className="pt-5">
         <Philosophy />
+      </div>
+      <div>
+        <User />
       </div>
     </div>
   );
