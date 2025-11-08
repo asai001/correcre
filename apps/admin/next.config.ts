0ad325c626d778bba6fdd92fc016117361cc19e0
genki asai <asai001@efficient-technology.com>
2025-11-08 09:25:09 +0900

    共通ライブラリの実装

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -2,6 +2,6 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
-  transpilePackages: ["@correcre/validation", "@correcre/types", "@correcre/adapters"],
+  transpilePackages: ["@correcre/validation", "@correcre/types", "@correcre/adapters", "@correcre/theme", "@correcre/lib"],
 };
 export default nextConfig;
---
+++ packages/lib/src/date/format.ts
@@ -0,0 +1,83 @@
+function getParts(d: Date, timeZone: string = "Asia/Tokyo") {
+  const dtf = new Intl.DateTimeFormat("en-CA", {
+    timeZone: timeZone,
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+  const { year, month, day, hour, minute, second } = Object.fromEntries(dtf.formatToParts(d).map((p) => [p.type, p.value]));
+  return { year, month, day, hour, minute, second };
+}
+
+/**
+ * 現在時刻を YYYY-MM 表記にフォーマットされた文字列で返す
+ *
+ * @param timezone
+ * @returns
+ */
+export function nowYYYYMM(timezone: string = "Asia/Tokyo"): string {
+  const { year, month } = getParts(new Date(), timezone);
+  return `${year}-${month}`;
+}
+
+/**
+ * 任意の時刻を YYYY-MM 表記にフォーマットされた文字列で返す
+ *
+ * @param date
+ * @param timezone
+ * @returns
+ */
+export function toYYYYMM(date: Date, timezone: string = "Asia/Tokyo"): string {
+  const { year, month } = getParts(date, timezone);
+  return `${year}-${month}`;
+}
+
+/**
+ * 現在時刻を YYYY-MM--DD 表記にフォーマットされた文字列で返す
+ *
+ * @param timezone
+ * @returns
+ */
+export function nowYYYYMMDD(timezone: string = "Asia/Tokyo"): string {
+  const { year, month, day } = getParts(new Date(), timezone);
+  return `${year}-${month}-${day}`;
+}
+
+/**
+ * 任意の時刻を YYYY-MM-DD 表記にフォーマットされた文字列で返す
+ *
+ * @param date
+ * @param timezone
+ * @returns
+ */
+export function toYYYYMMDD(date: Date, timezone: string = "Asia/Tokyo"): string {
+  const { year, month, day } = getParts(date, timezone);
+  return `${year}-${month}-${day}`;
+}
+
+/**
+ * 現在時刻を YYYY-MM-DD HH:mm:ss 表記にフォーマットされた文字列で返す
+ *
+ * @param timezone
+ * @returns
+ */
+export function nowYYYYMMDDHHmmss(timezone: string = "Asia/Tokyo"): string {
+  const { year, month, day, hour, minute, second } = getParts(new Date(), timezone);
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
+/**
+ * 任意の時刻を YYYY-MM-DD HH:mm:ss 表記にフォーマットされた文字列で返す
+ *
+ * @param date
+ * @param timezone
+ * @returns
+ */
+export function toYYYYMMDDHHmmss(date: Date, timezone: string = "Asia/Tokyo"): string {
+  const { year, month, day, hour, minute, second } = getParts(date, timezone);
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
