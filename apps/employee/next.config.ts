79c8c782cf658158031f36bc05d62541a71a3607
genki asai <asai001@efficient-technology.com>
2025-11-08 09:27:30 +0900

    共通ライブラリの実装

--- apps/employee/next.config.ts
+++ apps/employee/next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
-  transpilePackages: ["@correcre/validation", "@correcre/types", "@correcre/adapters", "@correcre/theme"],
+  transpilePackages: ["@correcre/validation", "@correcre/types", "@correcre/adapters", "@correcre/theme", "@correcre/lib"],
   experimental: {
     // apps/employee の外にある TS を読むため
     externalDir: true,
