801c026d58ecb47aab2ad533fa71e43d6c505885
genki asai <asai001@efficient-technology.com>
2026-05-06 14:21:12 +0900

    提携企業アプリログイン画面の背景変更

--- apps/merchant/src/app/(auth)/layout.tsx
+++ apps/merchant/src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ type AuthLayoutProps = Readonly<{
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="relative flex min-h-dvh w-full flex-col items-center overflow-x-hidden bg-[#CFE0FF]">
+    <div className="relative flex min-h-dvh w-full flex-col items-center overflow-x-hidden bg-[#FCEFF3]">
       {children}
     </div>
   );
